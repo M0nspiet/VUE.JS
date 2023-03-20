2428b734062e8bb5192596791092113e654900cd
M0nspiet <Kaliberovnik04@mail.ru>
2023-03-20 22:30:20 +0300

    1_wrk

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Counter from "./components/counter/Counter.vue";
+</script>
+
+<template>
+  <h1>Hello!</h1>
+  <Counter></Counter>
+</template>
+
+<style scoped>
+  h1{
+  color:red;
+  text-align: center;
+  }
+</style>
---
+++ src/components/counter/Counter.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { reactive,ref,watchEffect, watch } from 'vue';
+
+const counter = ref(0);
+const date = ref(new Date().toLocaleDateString());
+// const counterRef = reactive({
+//     counter: 0,
+// })
+
+function counterHandler(event){
+    counter.value++;
+}
+
+watch(
+    {},
+    ()=>{
+        setInterval(() =>{
+            date.value = new Date().toLocaleDateString();
+        },1000);
+    },
+    {immediate:true}
+)
+
+</script>
+
+<template>
+    <button @click="counterHandler">counter {{ counter }}</button>
+    <!-- <button @click="counterRef.counter++">counter ++</button>
+    <button @click="counterRef.counter--">counter --</button> -->
+    <!-- <h3>{{ counterRef.counter }}</h3> -->
+    <h1>{{ date }}</h1>
+</template>
+
+<style scoped></style>
