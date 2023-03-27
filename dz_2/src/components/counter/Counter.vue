5b6f78018850d8eec50998b96899884aaf561f3d
M0nspiet <Kaliberovnik04@mail.ru>
2023-03-27 23:18:14 +0300

    dz_2

---
+++ dz_2/src/components/counter/Counter.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { reactive,ref,watchEffect, watch } from 'vue';
+
+const counter = ref(0);
+const date = ref(new Date().toLocaleDateString());
+
+function counterHandler(event){
+    counter.value++;
+}
+
+watch(
+    {},
+    ()=>{
+        setInterval(() =>{
+            date.value = new Date().toLocaleDateString();
+        },1000);
+    },
+    {immediate:true}
+)
+
+</script>
+
+<template>
+    <button @click="counterHandler">counter {{ counter }}</button>
+    <h1>{{ date }}</h1>
+</template>
+
+<style scoped></style>
